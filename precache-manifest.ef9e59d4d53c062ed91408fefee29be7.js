17a188c0e5cf683646219fcb5ee3bf1335ff372b
Blackswan <tinle@BEP.local>
2020-02-06 08:40:17 +0700

    Updates

---
+++ precache-manifest.ef9e59d4d53c062ed91408fefee29be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59ee4fa3d0c9df2c9e45dd2aeb7423a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "926775573f037de8130b",
+    "url": "/monsters-rolodex/static/css/main.09bba4e5.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926775573f037de8130b",
+    "url": "/monsters-rolodex/static/js/main.338189cc.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
